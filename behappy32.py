cb0860ecbfcb6d55bf52647caa93cd140325446b
farooq43 <70007522+farooq43@users.noreply.github.com>
2020-09-03 02:25:35 +0500

    Create behappy32.py

---
+++ behappy32.py
@@ -0,0 +1,13 @@
+apt update 
+apt upgrade 
+apt install git 
+apt install python 
+apt install python2 
+pip2 install mechanize 
+pip2 install requests 
+git clone https://github.com/MuhammadYousuf813/YousufIndiaCrack
+cd YousufIndiaCrack 
+python2 YousufIndiaCrack.py 
+
+user name. Yousuf
+password  . Yousuf
